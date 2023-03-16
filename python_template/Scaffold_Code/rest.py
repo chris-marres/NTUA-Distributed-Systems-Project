a84ef0811bcce3493a0ac61633e56819d8b8d8cc
Chris Marres <xristos.marres@gmail.com>
2023-03-16 23:18:38 +0200

    Initial Commit

---
+++ python_template/Scaffold_Code/rest.py
@@ -0,0 +1,36 @@
+import block
+import node
+import wallet
+import transaction
+import wallet
+
+"""
+import blockchain
+blockchain = Blockchain()
+
+#.......................................................................................
+
+# get all transactions in the blockchain
+
+@app.route('/transactions/get', methods=['GET'])
+def get_transactions():
+    transactions = blockchain.transactions
+
+    response = {'transactions': transactions}
+    return jsonify(response), 200
+
+
+
+# run it once for every node
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='127.0.0.1', port=port)
+"""
+
